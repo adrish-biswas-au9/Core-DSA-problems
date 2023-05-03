5537e7d4baf83afca8250c3f2c27c146c85992c3
Adrish Biswas <adrishlinkin@gmail.com>
2023-05-03 06:23:43 +0530

    chore: update selection sort file, with better code

--- AfterLearning Functions/ArraySelectionSort.cpp
+++ AfterLearning Functions/ArraySelectionSort.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 void selectionSort(int arr[], int n)
 {
-  int smallest_for_each_step;
+  // int smallest_for_each_step;
   int index_for_smallest_for_each_step;
   int temp;
   for (int i = 0; i < n - 1; i++)
   {
-    smallest_for_each_step = arr[i];
+    // smallest_for_each_step = arr[i];
     index_for_smallest_for_each_step = i;
     for (int j = i; j < n; j++)
     {
-      //cout << j << "\n";
-      if (smallest_for_each_step > arr[j])
+      // cout << j << "\n";
+      if (arr[index_for_smallest_for_each_step] > arr[j])
       {
-        smallest_for_each_step = arr[j];
+        // smallest_for_each_step = arr[j];
         index_for_smallest_for_each_step = j;
       }
     }
     // cout << smallest_for_each_step << " " << index_for_smallest_for_each_step << " " << i << "\n";
-    temp = smallest_for_each_step;
+    temp = arr[index_for_smallest_for_each_step];
     arr[index_for_smallest_for_each_step] = arr[i];
     arr[i] = temp;
   }
