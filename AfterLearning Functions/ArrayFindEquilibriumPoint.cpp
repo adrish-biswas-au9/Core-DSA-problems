2eff8f011de2f73c1fad8973a98790b94d340b40
Adrish Biswas <adrishlinkin@gmail.com>
2023-05-07 05:04:49 +0530

    chore: optimise find equilibrium point in array file

--- AfterLearning Functions/ArrayFindEquilibriumPoint.cpp
+++ AfterLearning Functions/ArrayFindEquilibriumPoint.cpp
@@ -6,26 +6,41 @@ int equilibriumPoint(long long a[], int n)
   // https://practice.geeksforgeeks.org/problems/equilibrium-point-1587115620/1
   long long sumLeft = 0;
   long long sumRight = 0;
+  for (int i = 0; i < n; i++)
+  {
+    sumRight += a[i];
+  }
   int equilibriumPoint = -1;
   for (int i = 0; i < n; i++)
   {
-    sumLeft = 0;
-    sumRight = 0;
-    for (int j = 0; j < i; j++)
-    {
-      sumLeft += a[j];
-    }
-    for (int j = n - 1; j > i; j--)
-    {
-      sumRight += a[j];
-    }
+    sumRight -= a[i];
     if (sumLeft == sumRight)
     {
-      equilibriumPoint = i + 1;
-      break;
+      return i + 1;
     }
+    sumLeft += a[i];
   }
-  return equilibriumPoint;
+  return -1;
+  // int equilibriumPoint = -1;
+  // for (int i = 0; i < n; i++)
+  // {
+  //   sumLeft = 0;
+  //   sumRight = 0;
+  //   for (int j = 0; j < i; j++)
+  //   {
+  //     sumLeft += a[j];
+  //   }
+  //   for (int j = n - 1; j > i; j--)
+  //   {
+  //     sumRight += a[j];
+  //   }
+  //   if (sumLeft == sumRight)
+  //   {
+  //     equilibriumPoint = i + 1;
+  //     break;
+  //   }
+  // }
+  // return equilibriumPoint;
 }
 
 int main()
